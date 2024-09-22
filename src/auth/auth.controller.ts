66dbbec03a1832ad20f343f6ee915e6d91cb2e10
Alexander <juanperzezss@gmail.com>
2024-09-21 23:06:21 -0500

    agregar controlador al swager

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { AuthService } from './auth.service';
-
+import { ApiTags } from '@nestjs/swagger';
+@ApiTags('auth')
 @Controller('auth')
 export class AuthController {
 
