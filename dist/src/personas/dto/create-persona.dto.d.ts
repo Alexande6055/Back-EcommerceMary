fdfc0ee8d76d43fa09cf2f90e29d6e3809aa08c4
Alexander <juanperzezss@gmail.com>
2024-09-21 22:55:20 -0500

    agrega carpeta dist

---
+++ dist/src/personas/dto/create-persona.dto.d.ts
@@ -0,0 +1,9 @@
+export declare class CreatePersonaDto {
+    cedula: string;
+    nombre: string;
+    fecha_nacimiento: Date;
+    direccion: string;
+    foto: string;
+    celular: string;
+    id_usuario: number;
+}
---
+++ dist/src/personas/entities/persona.entity.d.ts
@@ -0,0 +1,11 @@
+export declare class Persona {
+    id_persona: number;
+    cedula: string;
+    nombre: string;
+    direccion: string;
+    foto: string;
+    celular: string;
+    fecha_nacimiento: Date;
+    isDebtor: boolean;
+    id_usuario: number;
+}
